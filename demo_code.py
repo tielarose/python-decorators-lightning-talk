daee016a9030438a4ed66f885d3817d95b03c9ec
Tiela Black-Law <tiela.rose@gmail.com>
2023-03-22 10:43:44 -0700

    Initial commit

---
+++ demo_code.py
@@ -0,0 +1,27 @@
+#########################################
+# EXAMPLE 1
+#########################################
+
+def print_hi():
+    print(f'Hi!')
+
+
+def ex_decorator(func):
+    def wrapper():
+        print('This happens before')
+        func()
+        print('This happens after')
+    return wrapper
+
+# # without the decorator
+# print_hi()
+# # prints 'Hi'
+
+# # reassign the function using the decorator
+# print_hi = ex_decorator(print_hi)
+
+# # now when I call print_hi, it is wrapped in the decorator
+# print_hi()
+# # prints 'This happens before'
+# # prints 'Hi
+# # prints 'This happens after'
