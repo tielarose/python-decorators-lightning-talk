e8f390f71486e9aedf1fdaa2150d217931fa14c2
Tiela Black-Law <tiela.rose@gmail.com>
2023-03-23 21:44:10 -0700

    Add example V2

--- demo_code.py
+++ demo_code.py
@@ -1,5 +1,5 @@
 #########################################
-# EXAMPLE 1
+# EXAMPLE V1
 #########################################
 
 def print_hi():
@@ -13,19 +13,23 @@ def ex_decorator(func):
         print('This happens after')
     return wrapper
 
-# # without the decorator
-# print_hi()
-# # prints 'Hi'
 
-# # reassign the function using the decorator
-# print_hi = ex_decorator(print_hi)
-
-# # now when I call print_hi, it is wrapped in the decorator
-# print_hi()
-# # prints 'This happens before'
-# # prints 'Hi
-# # prints 'This happens after'
+# reassign the function using the decorator
+print_hi = ex_decorator(print_hi)
 
 #########################################
-# EXAMPLE 2
+# EXAMPLE V2
 #########################################
+
+
+def ex_decorator(func):
+    def wrapper():
+        print('This happens before')
+        func()
+        print('This happens after')
+    return wrapper
+
+
+@ex_decorator
+def print_hi():
+    print(f'Hi!')
