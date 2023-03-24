194138c60171a1da70444b2ad0c918c0bcca56d8
Tiela Black-Law <tiela.rose@gmail.com>
2023-03-23 22:15:46 -0700

    Add example V4

--- demo_code.py
+++ demo_code.py
@@ -2,20 +2,20 @@
 # EXAMPLE V1
 #########################################
 
-def print_hi():
-    print(f'Hi!')
+# def print_hi():
+#     print(f'Hi!')
 
 
-def ex_decorator(func):
-    def wrapper():
-        print('This happens before')
-        func()
-        print('This happens after')
-    return wrapper
+# def ex_decorator(func):
+#     def wrapper():
+#         print('This happens before')
+#         func()
+#         print('This happens after')
+#     return wrapper
 
 
-# reassign the function using the decorator
-print_hi = ex_decorator(print_hi)
+# # reassign the function using the decorator
+# print_hi = ex_decorator(print_hi)
 
 #########################################
 # EXAMPLE V2
@@ -39,19 +39,38 @@ print_hi = ex_decorator(print_hi)
 #########################################
 
 
+# def ex_decorator(func):
+#     def wrapper(*args, **kwargs):
+#         print('This happens before')
+#         func(*args, **kwargs)
+#         print('This happens after')
+#     return wrapper
+
+
+# @ex_decorator
+# def greet_someone(name):
+#     print(f'Hi!, {name}!')
+
+
+# @ex_decorator
+# def greet_two_people(name1, name2):
+#     print(f'Hi, {name1} and {name2}!')
+
+#########################################
+# EXAMPLE V4
+#########################################
+
+
 def ex_decorator(func):
     def wrapper(*args, **kwargs):
         print('This happens before')
-        func(*args, **kwargs)
+        the_function = func(*args, **kwargs)
         print('This happens after')
+        return the_function
     return wrapper
 
 
 @ex_decorator
-def greet_someone(name):
-    print(f'Hi!, {name}!')
-
-
-@ex_decorator
-def greet_two_people(name1, name2):
-    print(f'Hi, {name1} and {name2}!')
+def return_a_greeting(name):
+    print('The greeting was created!')
+    return f'Hi, {name}!'
