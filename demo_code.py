6776ebda08f2c5d661da44c83d21e5b9e1342530
Tiela Black-Law <tiela.rose@gmail.com>
2023-03-23 21:54:58 -0700

    Add example V3

--- demo_code.py
+++ demo_code.py
@@ -22,14 +22,36 @@ print_hi = ex_decorator(print_hi)
 #########################################
 
 
+# def ex_decorator(func):
+#     def wrapper():
+#         print('This happens before')
+#         func()
+#         print('This happens after')
+#     return wrapper
+
+
+# @ex_decorator
+# def print_hi():
+#     print(f'Hi!')
+
+#########################################
+# EXAMPLE V3
+#########################################
+
+
 def ex_decorator(func):
-    def wrapper():
+    def wrapper(*args, **kwargs):
         print('This happens before')
-        func()
+        func(*args, **kwargs)
         print('This happens after')
     return wrapper
 
 
 @ex_decorator
-def print_hi():
-    print(f'Hi!')
+def greet_someone(name):
+    print(f'Hi!, {name}!')
+
+
+@ex_decorator
+def greet_two_people(name1, name2):
+    print(f'Hi, {name1} and {name2}!')
